0e6546c32467c66fc03d7c73308938719ff644da
gusevaEA <115579420+gusevaEA@users.noreply.github.com>
2022-12-18 18:38:55 +0300

    Лабораторная работа 1

--- Лабораторная 1/main.py
+++ Лабораторная 1/main.py
@@ -1,20 +1,52 @@
-from typing import Union
-
-
-class Glass:
-    def __init__(self, capacity_volume: Union[int, float], occupied_volume: Union[int, float]):
-        ...  # TODO инициализировать объект "Стакан"
-
-class passport:
+# TODO Написать 3 класса с документацией и аннотацией типов
+class Passport:
     def __init__(self,nationality, name,date):
         # Атрибутам присваиваются значения аргументов конструктора объекта
         self.nationality = nationality
         self.name = name
         self.date = date
 
+    def show_status(self):
+        print(f'{self.name} arrived')
+
+class Book:
+    def __init__(self,name,author,country):
+        self.name = name
+        self.author = author
+        self.country = country
+
     def read(self):
         print(f'{self.name} read')
-if __name__ == "__main__":
-    ...  # TODO инициализировать два объекта типа Glass
 
-    # TODO попробовать инициализировать не корректные объекты
+    def get_author(self):
+        return self.author
+
+class Animals:
+    def __init__(self,type,color,weight):
+        self.type = type
+        self.color = color
+        self.weight = weight
+
+    def get_weight_kg(self):
+        """
+        Класс Passport,Book,Animals
+        Извлечение
+
+        :return:
+        """
+        return self.weight/1000
+
+    def read(self):
+        print(f'{self.name}read')
+
+if __name__ == "__main__": #TODO работоспособность экземпляров класса проверить с помощью doctest
+    dog = Animals("sobaka","white",5200)
+    dog_weight = dog.get_weight_kg()
+    print(dog_weight)
+
+    book_1 = Book("IDIOT","Dostoevskiy","Russia")
+    book_1_author = book_1.get_author()
+    print(book_1_author)
+
+    passport_1 = Passport("Russian","Sasha","10.01.2001")
+    passport_1.show_status()
