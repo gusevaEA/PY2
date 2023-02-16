e272390418f7745b6d3d36d8903f6b78ea6056e4
gusevaEA <115579420+gusevaEA@users.noreply.github.com>
2023-02-16 21:42:43 +0300

    Лабораторная работа 3

--- Лабораторная 3/main.py
+++ Лабораторная 3/main.py
@@ -1,8 +1,16 @@
 class Book:
     """ Базовый класс книги. """
     def __init__(self, name: str, author: str):
-        self.name = name
-        self.author = author
+        self.__name = name
+        self.__author = author
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def author(self):
+        return self.__author
 
     def __str__(self):
         return f"Книга {self.name}. Автор {self.author}"
@@ -11,21 +19,39 @@ class Book:
         return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
 
 
-class PaperBook:
+class PaperBook(Book):
     def __init__(self, name: str, author: str, pages: int):
-        self.name = name
-        self.author = author
+        super().__init__(name, author)
         self.pages = pages
 
-    def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+    @property
+    def pages(self):
+        return self.pages
+
+    @pages.setter
+    def pages(self, new_pages):
+        if isinstance(new_pages, int):
+            self.pages = new_pages
+        else:
+            raise ValueError
 
 
-class AudioBook:
+class AudioBook(Book):
     def __init__(self, name: str, author: str, duration: float):
-        self.name = name
-        self.author = author
+        super().__init__(name, author)
         self.duration = duration
 
-    def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+    @property
+    def duration(self):
+        return self.duration
+
+    @duration.setter
+    def duration(self, new_duration):
+        if isinstance(new_duration, float):
+            self.duration = new_duration
+        else:
+            raise ValueError
+
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name=--{self.name!r}, author={self.author!r}, duration={self.duration!r})"
